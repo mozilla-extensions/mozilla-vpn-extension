278882eb056b8f9e226e0ce4ad21bbc176eae90e
Sebastian Streich <strseb@users.noreply.github.com>
2024-07-08 19:37:32 +0200

    Add Lit@3.14 (#8)
    - Add a conda env file... so i remember to not use node 16
    - Add docs on how to generate a lit bundle
    - Add lit@3.1.4
    - P1 FEATURE: Add a clock!

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import {html, render} from "../vendor/lit-all.min.js"
+
+const template = (time) => html`
+  <pre>${time}<pre>
+`
+
+const renderTime = ()=>{
+    const now = new Date()
+    render(template(
+      `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+    ), document.body);
+}
+
+window.setInterval(renderTime,1000);
+renderTime();
 
 (() => {
   console.log("Hello from the popup script");
