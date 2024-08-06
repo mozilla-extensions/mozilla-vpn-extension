d59bcb09bf8fed25bd82312490c5607ef30b747f
Sebastian Streich <streich.mobile@gmail.com>
2024-08-06 18:38:47 +0200

    Make the request handler reactive

--- src/background/requestHandler.js
+++ src/background/requestHandler.js
@@ -6,6 +6,10 @@ import { Component } from "./component.js";
 import { Logger } from "./logger.js";
 import { Utils } from "../shared/utils.js";
 import { VPNController, VPNState } from "./vpncontroller/index.js";
+import { ProxyHandler } from "./proxyHandler/proxyHandler.js";
+import { ProxyUtils } from "./proxyHandler/proxyUtils.js";
+
+import { propertySum } from "../shared/property.js";
 
 const log = Logger.logger("RequestHandler");
 let self;
@@ -18,55 +22,61 @@ export class RequestHandler extends Component {
    *
    * @param {*} receiver
    * @param {VPNController} controller
+   *  @param {ProxyHandler} proxyHandler
    */
   constructor(receiver, controller, proxyHandler) {
     super(receiver);
     this.controller = controller;
+    /** @type {ProxyHandler}  */
     this.proxyHandler = proxyHandler;
+    this.active = false;
+
+    propertySum(
+      controller.state,
+      proxyHandler.siteContexts,
+      (currentVPNState, currentSiteContextMap) => {
+        return resolveSiteContext(currentSiteContextMap, currentVPNState);
+      }
+    ).subscribe((proxyMap) => {
+      console.log(proxyMap);
+      this.onNewProxyMap(proxyMap);
+    });
+
     self = this;
+  }
+
+  /**
+   *
+   * @param {Map<string, Array<browser.proxy.proxyInfo>>} proxyMap
+   */
+  onNewProxyMap(proxyMap) {
+    this.proxyMap = proxyMap;
+    if (proxyMap.size === 0 && this.active) {
+      this.removeRequestListener();
+      return;
+    }
+    if (!this.active && proxyMap.size > 0) {
+      this.addRequestListener();
+    }
   }
 
   /** @type {VPNState | undefined} */
   controllerState;
+  /** @type {Map<string, SiteContext>} */
   siteContexts;
 
   async init() {
     log("Initiating RequestHandler");
-
-    this.controller.state.subscribe((s) => {
-      this.controllerState = s;
-      this.addOrRemoveRequestListener();
-    });
-
-    this.proxyHandler.siteContexts.subscribe((siteContexts) => {
-      this.siteContexts = siteContexts;
-      if (this.siteContexts.size === 0) {
-        return this.removeRequestListener();
-      }
-      return this.addOrRemoveRequestListener();
-    });
   }
 
   addRequestListener() {
-    log("Adding request listener");
-
-    browser.proxy.onRequest.addListener(this.interceptRequests, {
+    console.log(
+      `Starting listening for requests, active rules ${this.proxyMap.size}`
+    );
+    browser.proxy.onRequest.addListener(this.interceptRequests.bind(this), {
       urls: ["<all_urls>"],
     });
-  }
-
-  async addOrRemoveRequestListener() {
-    const s = this.controllerState.state;
-
-    switch (s) {
-      case "Enabled":
-        this.addRequestListener();
-        break;
-      case "Disabled":
-        this.removeRequestListener();
-        break;
-      // TODO what is the right behavior on "Unavailable"?
-    }
+    this.active = true;
   }
 
   /**
@@ -76,14 +86,13 @@ export class RequestHandler extends Component {
    */
   async interceptRequests(requestInfo) {
     let { url, originUrl } = requestInfo;
-
     for (let urlString of [url, originUrl]) {
       if (urlString) {
         const parsedHostname = await Utils.getFormattedHostname(urlString);
-        const siteContext = self.siteContexts.get(parsedHostname);
-        if (siteContext) {
-          log("Intercepting web request. Redirect to local proxy.");
-          return [...siteContext.proxyInfo];
+        const proxyInfo = this.proxyMap.get(parsedHostname);
+        if (proxyInfo) {
+          log(`Sending ${parsedHostname} to ${proxyInfo[0].host}`);
+          return proxyInfo;
         }
       }
     }
@@ -94,6 +103,47 @@ export class RequestHandler extends Component {
 
   removeRequestListener() {
     log("Removing request listener");
-    return browser.proxy.onRequest.removeListener(this.interceptRequests);
+    const ok = browser.proxy.onRequest.removeListener(this.interceptRequests);
+    this.active = false;
+    return ok;
   }
 }
+
+/**
+ * @typedef {Object} browser.proxy.proxyInfo
+ * @property {string} type - The connection type
+ * @property {string?} [username] - The username for the connection (optional).
+ * @property {string?} [password] - The password for the connection (optional).
+ * @property {string} host - The host for the connection (required).
+ * @property {string} port - The port for the connection (required).
+ *
+ * @typedef {import("./proxyHandler/siteContext.js").SiteContext} SiteContext
+ *
+ */
+
+/**
+ *
+ * Maps (VPNState+SiteContextMap) to a Map<origin:selectedProxies>
+ *
+ * @param {Map<string, SiteContext>} siteContexts
+ * @param {VPNState} vpnState
+ * @returns {Map<string, Array<browser.proxy.proxyInfo>>}
+ */
+export const resolveSiteContext = (siteContexts, vpnState) => {
+  /** @type {Map<string, Array<browser.proxy.proxyInfo>>} */
+  const result = new Map();
+  const localProxy = vpnState.loophole
+    ? [ProxyUtils.parseProxy(vpnState.loophole)]
+    : [];
+  siteContexts.forEach((ctx, origin) => {
+    if (ctx.excluded) {
+      result.set(origin, { ...localProxy });
+    } else {
+      result.set(
+        origin,
+        ProxyUtils.getProxies(ctx.countryCode, ctx.cityCode, vpnState.servers)
+      );
+    }
+  });
+  return result;
+};
