d0aaf2b0d8a65c06eb12a98cce4373c06529fbe7
Lesley Norton <lesley@mozilla.com>
2024-08-07 13:55:49 -0500

    Avec Prettier

--- src/background/tabHandler.js
+++ src/background/tabHandler.js
@@ -93,7 +93,6 @@ export class TabHandler extends Component {
     return browser.pageAction.hide(currentTab.id);
   }
 
-
   popupData() {
     return {
       type: "tabInfo",
--- src/ui/pageAction/pageAction.js
+++ src/ui/pageAction/pageAction.js
@@ -2,19 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-import {
-  html,
-  css,
-  LitElement,
-} from "../../vendor/lit-all.min.js";
+import { html, css, LitElement } from "../../vendor/lit-all.min.js";
 
 import { vpnController, proxyHandler } from "../browserAction/backend.js";
 
 import { Utils } from "../../shared/utils.js";
 
 import { fontSizing, resetSizing } from "../../components/styles.js";
-
 
 /**
  * This is the Component for the Page Action Popup.
@@ -47,7 +41,7 @@ export class PageActionPopup extends LitElement {
   }
 
   render() {
-    const excluded = this._siteContext?.excluded; 
+    const excluded = this._siteContext?.excluded;
     const scheme =
       window.matchMedia &&
       !!window.matchMedia("(prefers-color-scheme:dark)").matches
@@ -55,12 +49,10 @@ export class PageActionPopup extends LitElement {
         : "dark";
 
     const contextDescriptionText = () => {
-      return excluded ? "Protection for this site:"
-      : "Location for this site:";
+      return excluded ? "Protection for this site:" : "Location for this site:";
     };
 
     const getLocationText = () => {
-
       if (excluded) {
         return "Off";
       }
@@ -74,11 +66,11 @@ export class PageActionPopup extends LitElement {
       return excluded
         ? `./../../assets/logos/logo-${scheme}-excluded.svg`
         : `./../../assets/flags/${this._siteContext?.countryCode}.png`;
-    }
+    };
 
     const removeContext = () => {
       proxyHandler.removeContextForOrigin(this._siteContext.origin);
-    }
+    };
 
     const resetButtonClasses = () => {
       return excluded ? "primary" : "secondary";
@@ -86,7 +78,7 @@ export class PageActionPopup extends LitElement {
 
     const resetButtonContent = () => {
       return excluded ? "Turn on" : "Remove custom location";
-    }
+    };
 
     return html`
       <h1>Mozilla VPN</h1>
@@ -95,16 +87,21 @@ export class PageActionPopup extends LitElement {
       <div class="flex">
         <img id="context-img" src="${getSrc()}" />
         <p id="context-location">${getLocationText()}</p>
-        </div>
+      </div>
       <div class="flex">
-
-        <button class="${resetButtonClasses()}" id="removeContext" @click="${removeContext}">${resetButtonContent()}</button>
+        <button
+          class="${resetButtonClasses()}"
+          id="removeContext"
+          @click="${removeContext}"
+        >
+          ${resetButtonContent()}
+        </button>
       </div>
     `;
   }
 
   static styles = css`
-  ${fontSizing}${resetSizing}
+    ${fontSizing}${resetSizing}
 
     h1 {
       font-size: 13px;
@@ -114,29 +111,29 @@ export class PageActionPopup extends LitElement {
       text-align: center;
       border-bottom: 1px solid rgb(173, 173, 173, 0.8);
     }
-    
+
     .flex {
       display: flex;
     }
-    
+
     #context-description {
       margin-block: 4px 0;
     }
-    
+
     #context-img {
       margin-inline-end: 0.5rem;
       block-size: 1rem;
       margin-block: auto;
     }
-    
+
     /* #context-status {
       padding: 1rem;
     } */
-    
+
     p {
       margin-block: 0.5rem;
     }
-    
+
     button {
       margin-block-start: 1rem;
       margin-inline: auto 0;
@@ -148,32 +145,32 @@ export class PageActionPopup extends LitElement {
       background: none;
       transition: background 0.1s ease;
     }
-    
+
     .primary {
       background: var(--button-primary-bg-default);
       color: var(--button-primary-text-color);
     }
-    
+
     .primary:hover {
       background: var(--button-primary-bg-hover);
     }
-    
+
     .primary:active {
       background: var(--button-primary-bg-active);
     }
-    
+
     .secondary {
       background: var(--button-secondary-bg-default);
       color: var(--button-secondary-text-color);
     }
-    
+
     .secondary:hover {
       background: var(--button-secondary-bg-hover);
     }
-    
+
     .secondary:active {
       background: var(--button-secondary-bg-active);
     }
-  `
+  `;
 }
 customElements.define("page-action-popup", PageActionPopup);
