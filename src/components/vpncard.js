6362e2657cd7488d25a9308b07d109510e945c55
Sebastian Streich <streich.mobile@gmail.com>
2024-09-02 16:48:37 +0200

    format!!

--- src/components/vpncard.js
+++ src/components/vpncard.js
@@ -2,12 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {
-  html,
-  css,
-  LitElement,
-  classMap,
-} from "../vendor/lit-all.min.js";
+import { html, css, LitElement, classMap } from "../vendor/lit-all.min.js";
 import { tr } from "../shared/i18n.js";
 import { resetSizing } from "./styles.js";
 
