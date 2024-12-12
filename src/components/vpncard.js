53957b67d2dfb48539cb2e3aa4439f4df0864b89
Lesley Norton <lesley@mozilla.com>
2024-12-12 17:02:05 -0600

    Add in-copy 'Learn more' link to vpncard subLine

--- src/components/vpncard.js
+++ src/components/vpncard.js
@@ -12,7 +12,7 @@ import {
   ref,
 } from "../vendor/lit-all.min.js";
 import { tr } from "../shared/i18n.js";
-import { resetSizing, fontStyling, positioner } from "./styles.js";
+import { resetSizing, fontStyling, positioner, inCopyLink } from "./styles.js";
 
 import { VPNState } from "../background/vpncontroller/states.js";
 import "./mz-rings.js";
@@ -169,9 +169,21 @@ export class VPNCard extends LitElement {
   }
 
   static subline(enabled, stability, clientIsConnected) {
+    const onLinkClick = () => {
+      browser.tabs.create({
+        url: "https://support.mozilla.org/kb/get-started-mozilla-vpn-extension?utm_medium=mozilla-vpn&utm_source=vpn-extension",
+      });
+      window.close();
+    };
+
     if (!enabled) {
       return clientIsConnected
-        ? html`<p class="subline ext-is-off">${tr("extensionVpnIsOff")}</p>`
+        ? html`<p class="subline ext-is-off">
+            ${tr("extensionVpnIsOff")}
+            <a class="in-copy-link" @click=${onLinkClick} href=""
+              >${tr("learnMore")}</a
+            >
+          </p>`
         : null;
     }
     const errorSvg = html`
@@ -206,7 +218,7 @@ export class VPNCard extends LitElement {
   }
 
   static styles = css`
-    ${resetSizing}${fontStyling}${positioner}
+    ${resetSizing}${fontStyling}${positioner}${inCopyLink}
 
     :host {
       font-size: 1rem;
