98d37e1dccc288c4bcb6aa190a33c3b5cb386442
Lesley Norton <lesley@mozilla.com>
2025-04-09 09:37:28 -0700

    Low hanging a11y fixes (#230)
    This PR contains a few quick fixes for issues unearthed in the recent
    accessibility audit.
    Fixes FXVPN-429, 431, 434, 436, 427, 433, 437.

--- src/components/message-screen.js
+++ src/components/message-screen.js
@@ -61,8 +61,12 @@ export class MessageScreen extends LitElement {
       <vpn-titlebar title=${this.titleHeader}></vpn-titlebar>
       <div class="inner">
         <div class="upper">
-          <img class="${this.identifier}" src="/assets/img/${this.img}" />
-          <h1>${this.heading}</h1>
+          <img
+            class="${this.identifier}"
+            src="/assets/img/${this.img}"
+            aria-hidden="true"
+          />
+          <h2>${this.heading}</h2>
           <slot></slot>
         </div>
         ${when(
@@ -225,7 +229,7 @@ export class MessageScreen extends LitElement {
       background: var(--action-button-color);
     }
 
-    h1 {
+    h2 {
       font-family: var(--font-family-bold);
       margin-block: 16px;
       color: #3d3d3d;
@@ -267,7 +271,7 @@ export class MessageScreen extends LitElement {
     }
 
     @media (prefers-color-scheme: dark) {
-      h1 {
+      h2 {
         color: #ffffff;
       }
 
